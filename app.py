e3eabfe7cc3b18a13e1738d928647b7551d291b4
Sarah <sarah.schwick@gmail.com>
2025-05-01 17:44:30 +0200

    add project structure, add get_media_type

--- app.py
+++ app.py
@@ -1,41 +1,35 @@
-from flask import Flask, request  # Importa Flask e request per gestire le richieste HTTP.
-import os  # Modulo per gestire variabili di ambiente, come la porta.
-import logging  # Modulo per la gestione dei log.
+from flask import Flask, request  
+import os  
+import logging  
 
-# Creazione dell'app Flask
+NOTION_API_KEY = os.getenv("NOTION_API_KEY")
+DATABASE_ID = ""
+
+
 app = Flask(__name__)
 
-# Configurazione del logger
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)  # Crea un logger per il modulo corrente.
 
-# Definizione route principale (GET "/")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @app.route('/', methods=['GET']) 
 def home():
     return "Webhook attivo!"  
 
-# Definizione route per webhook Notion (POST "/notion-webhook")
 @app.route('/notion-webhook', methods=['POST']) 
 def handle_webhook():
-    # Logga le intestazioni della richiesta
     logger.info(f"Headers: {request.headers}")
     
-    # Logga il corpo della richiesta
     logger.info(f"Body: {request.get_data(as_text=True)}")
     
     data = request.json
     
-    # Logga i dati ricevuti 
     logger.info(f"Ricevuto: {data}")
     
     return '', 200
 
 # Blocco che viene eseguito quando l'app viene avviata
 if __name__ == '__main__':
-    # Ottiene la porta da una variabile di ambiente, se disponibile (utile per i servizi in produzione).
-    # Se la variabile di ambiente non è presente, usa la porta 5000 (valore di default di Flask).
     port = int(os.environ.get("PORT", 5000))
     
-    # Avvia il server Flask, in ascolto su tutte le interfacce di rete (host='0.0.0.0'),
-    # sulla porta specificata dalla variabile di ambiente o sul valore di default.
     app.run(host='0.0.0.0', port=port)
