aa544b0cbcb9a44ca5fa05f81f4b017ae3305f06
Sarah <sarah.schwick@gmail.com>
2025-05-01 12:55:15 +0200

    bind Flask to correct port for Render

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, request
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 
