8d0874abb53adb06cd278820c9b1003cd648af9e
Sarah <sarah.schwick@gmail.com>
2025-05-02 10:29:43 +0200

    fix notion version

--- notion_client.py
+++ notion_client.py
@@ -5,7 +5,7 @@ def get_page_data(page_id):
     url = f"https://api.notion.com/v1/pages/{page_id}"
     headers = {
         "Authorization": f"Bearer {config.NOTION_TOKEN}",
-        "Notion-Version": {config.NOTION_VERSION}
+        "Notion-Version": config.NOTION_VERSION
     }
 
     res = requests.get(url, headers=headers)
